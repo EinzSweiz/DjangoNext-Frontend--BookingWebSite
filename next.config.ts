b2ccf14703306a518943e630523d3d34c91cacf5
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-11-20 06:57:33 -0600

    prepared frontend part for prod added new env prod

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [{
+    remotePatterns: [
+      {
       protocol: 'http',
       hostname: 'localhost',
       port: '8010',
       pathname: '/**'
-    }]
+      },
+      {
+        protocol: 'http',
+        hostname: '165.22.76.137',
+        port: '1337',
+        pathname: '/**'
+      }
+  ]
   }
 };
 
