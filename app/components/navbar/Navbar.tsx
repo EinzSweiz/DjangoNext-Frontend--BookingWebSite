de948e791509d4cb421b6d83d1388e3a136b65f2
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-11-22 12:33:10 -0600

    refactored navbar and changed logo

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -6,33 +6,29 @@ import AddPropertyButton from "./AddPropertyButton";
 import { getUserId } from "@/app/lib/actions";
 
 const Navbar = async () => {
-  const userId = await getUserId()
+  const userId = await getUserId();
   return (
     <nav className="w-full fixed top-0 left-0 py-6 border-b bg-white z-10">
       <div className="max-w-[2000px] mx-auto px-6">
         <div className="flex justify-between items-center">
           <Link href="/">
             <Image
-              src="/logo.jpg"
+              className="transition-transform duration-300 ease-in-out transform hover:scale-110 border rounded-full border-black"
+              src="/logo.png"
               alt="Logo"
               width={100}
               height={38}
-              priority // Optional: this will prioritize loading the image
+              priority
             />
           </Link>
           <div className="flex space-x-6">
             <SearchFilters />
           </div>
           <div className="flex items-center space-x-6">
-            <AddPropertyButton userid={userId}/>
-
-            <UserNav 
-            userId={userId}
-            />
-
+            <AddPropertyButton userid={userId} />
+            <UserNav userId={userId} />
           </div>
         </div>
-
       </div>
     </nav>
   );
