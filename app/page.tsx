036b894dc9d21d2eb6c68be6f6aa5bdd8693bffb
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-11-23 13:26:12 -0600

    added Suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Categories from "./components/Categories";
 import Footer from "./components/footer/footer";
 import PropertyList from "./components/properties/PropertyList";
+import { Suspense } from "react";  // Import Suspense
 
 export default function Home() {
   return (
@@ -8,7 +9,10 @@ export default function Home() {
       <Categories />
       
       <div className="mt-4 my-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6 flex-grow pl-6 pr-6">
-        <PropertyList />
+        {/* Wrap PropertyList with Suspense */}
+        <Suspense fallback={<div>Loading properties...</div>}>
+          <PropertyList />
+        </Suspense>
       </div>
 
       <Footer />
