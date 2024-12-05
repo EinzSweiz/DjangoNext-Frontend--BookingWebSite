97efaa2433aceef006d7edca58323102c2082080
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-12-05 09:37:31 -0600

    changed next config

--- next.config.ts
+++ next.config.ts
@@ -4,18 +4,30 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-      protocol: 'http',
-      hostname: 'localhost',
-      port: '8010',
-      pathname: '/**'
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8010',  // Assuming you're testing on localhost with this port
+        pathname: '/**'
+      },
+      {
+        protocol: 'http',
+        hostname: 'www.diplomaroad.pro',
+        port: '1773',  // If you're using this port for your backend
+        pathname: '/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.diplomaroad.pro',
+        port: '',  // If you're using this port for your backend
+        pathname: '/**'
       },
       {
         protocol: 'http',
         hostname: '165.22.76.137',
-        port: '1337',
+        port: '1773',  // Your external server IP
         pathname: '/**'
       }
-  ]
+    ]
   }
 };
 
