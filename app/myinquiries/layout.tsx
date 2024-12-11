6d198e2f19a41583e807ae4aa9758fdaa0c15aa8
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-12-11 05:00:26 -0600

    refactored code and added own layout

--- app/myinquiries/layout.tsx
+++ app/myinquiries/layout.tsx
@@ -1,16 +1,48 @@
 import React from "react";
 import CustomNavbar from "../components/navbar/CustomNavbar";
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+import { ThemeProvider } from "../components/theme-provider";
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "DiplomaPro",
+  description: "We Write, You Graduate",
+  icons: {
+    icon: "/favicon.png", // Path to your icon
+  },
+};
+
 export default function InquiryLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <html lang="en">
-      <body>
-        <CustomNavbar />
-        <main>{children}</main>
-        {/* No Footer */}
+    <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Meta tags for SEO or social media */}
+      </head>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <CustomNavbar />
+          <div className="pt-32">{children}</div>
+        </ThemeProvider>
       </body>
     </html>
   );
