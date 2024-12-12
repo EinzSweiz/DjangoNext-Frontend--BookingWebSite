f62ebc60e7ddadcba7a85b954392662b10cdba4e
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-12-12 07:22:24 -0600

    want to do them the same size for this reduce the length of text in preview

--- app/myinquiries/page.tsx
+++ app/myinquiries/page.tsx
@@ -62,10 +62,10 @@ const MyInquiries = () => {
                                         />
                                     </div>
                                     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                                        {truncateText(inquiry.subject, 50)}
+                                        {truncateText(inquiry.subject, 25)}
                                     </h5>
                                     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                                        {truncateText(inquiry.message, 100)}
+                                        {truncateText(inquiry.message, 50)}
                                     </p>
                                     <div className="mb-3">
                                         <span
