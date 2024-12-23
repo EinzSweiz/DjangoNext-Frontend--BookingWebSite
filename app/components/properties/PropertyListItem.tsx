4c64c323f9afad6e6ff548bc42fcec6fdf8090aa
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-12-23 11:05:04 -0600

    refactored code found bug inside the landlords properties(demo fix first try)

--- app/components/properties/PropertyListItem.tsx
+++ app/components/properties/PropertyListItem.tsx
@@ -16,7 +16,7 @@ const PropertyListItem: React.FC<PropertyProps> = ({
     return (
         <div
         className="cursor-pointer"
-        onClick={() => router.push(`properties/${property.id}`)}
+        onClick={() => router.push(`/properties/${property.id}`)}
     >
         <div className="relative overflow-hidden aspect-square rounded-xl">
             <Image
