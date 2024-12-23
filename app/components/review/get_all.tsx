a11c1681ae6f5f7918a43a937ffabcdff96bd9d7
EinzSweiz <riad.sultanov.1999@gmail.com>
2024-12-23 08:26:03 -0600

    added opening report part only if user was authenticated

--- app/components/review/get_all.tsx
+++ app/components/review/get_all.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import apiService from "@/app/services/apiService";
 import ReviewDropdown from "./DropDown";
 import useReviewModal from "@/app/hooks/useReviewModal";
+import { getUserId } from "@/app/lib/actions";
 import { useRouter } from "next/navigation";
 interface User {
     id: string;
@@ -27,9 +28,18 @@ const GetAllReviews = ({ propertyId }: { propertyId: string }) => {
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(1);
     const router = useRouter()
+    const [userId, setUserId] = useState<string | null>(null);
     const [selectedReview, setSelectedReview] = useState<Review | null>(null);
 
     const reviewModal = useReviewModal();
+
+    useEffect(() => {
+        const fetchUserId = async () => {
+            const id = await getUserId()
+            setUserId(id)
+        }
+        fetchUserId()
+    }, [])
 
     const fetchReviews = async (page: number) => {
         try {
@@ -110,7 +120,7 @@ const GetAllReviews = ({ propertyId }: { propertyId: string }) => {
                                         {review.text}
                                     </p>
                                 </div>
-                                <ReviewDropdown onReport={() => reviewModal.open(review)} />
+                                {userId && <ReviewDropdown onReport={() => reviewModal.open(review)} />}
                             </motion.li>
                         ))}
                     </AnimatePresence>
